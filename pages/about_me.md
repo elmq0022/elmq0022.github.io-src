9435148f9f52ec728789fad43736cd72491f0b88
zero_loss <elmq0022@umn.edu>
2017-04-09 13:28:34 -0500

    changed to blue-penguin theme

---
+++ pages/about_me.md
@@ -0,0 +1,3 @@
+Title: About Me
+Date: 2017-04-09
+Hi I'm Aaron!
--- pelicanconf.py
+++ pelicanconf.py
@@ -26,10 +26,46 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Github', r'https://github.com/elmq0022'),
+          ('LinkedIn', r'https://www.linkedin.com/in/aaron-elmquist-99800839/'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+
+# Theme Specific Settings
+THEME = 'blue-penguin'
+
+SITETITLE = 'Leaked Memories'
+SITESUBTITLE = 'Sharing My Adventures in Coding'
+
+# provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
+TAGS_URL           = 'tags'
+TAGS_SAVE_AS       = 'tags/index.html'
+AUTHORS_URL        = 'authors'
+AUTHORS_SAVE_AS    = 'authors/index.html'
+CATEGORIES_URL     = 'categories'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+ARCHIVES_URL       = 'archives'
+ARCHIVES_SAVE_AS   = 'archives/index.html'
+ABOUT_ME_URL       = 'about-me'
+ABOUT_ME_SAVE_AS   = 'about-me/index.html'
+
+
+# use those if you want pelican standard pages to appear in your menu
+MENU_INTERNAL_PAGES = (
+    ('Tags', TAGS_URL, TAGS_SAVE_AS),
+    ('Authors', AUTHORS_URL, AUTHORS_SAVE_AS),
+    ('Categories', CATEGORIES_URL, CATEGORIES_SAVE_AS),
+    ('Archives', ARCHIVES_URL, ARCHIVES_SAVE_AS),
+    ('About Me', ABOUT_ME_URL, ABOUT_ME_SAVE_AS),
+)
+
+# additional menu 
+# MENUITEMS = (
+#     ('GitHub', 'https://github.com/'),
+#     ('Linux Kernel', 'https://www.kernel.org/'),
+# )
+
